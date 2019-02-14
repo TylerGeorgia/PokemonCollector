ea8ad45216b0de4b4a4dcff692b45f001aa22478
unknown <georgiacs91@gmail.com>
2019-02-13 20:15:52 -0500

    Services getUserById() and getLeaderBoard() finished

--- PokemonCollector/src/main/java/com/revature/model/Pokedex.java
+++ PokemonCollector/src/main/java/com/revature/model/Pokedex.java
@@ -1,5 +1,24 @@
 package com.revature.model;
 
-public class Pokedex {
+import java.util.List;
 
+public class Pokedex {
+	
+	private User owner;
+	private List<Pokemon> ownedPokemon;
+	
+	public User getOwner() {
+		return owner;
+	}
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+	public List<Pokemon> getOwnedPokemon() {
+		return ownedPokemon;
+	}
+	public void setOwnedPokemon(List<Pokemon> ownedPokemon) {
+		this.ownedPokemon = ownedPokemon;
+	}
+	
+	
 }
